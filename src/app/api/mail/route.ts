d606316020e21d34eed23ec405bd02a95515c903
Mrudauskas <marcus@rudauskas.de>
2023-08-03 15:44:12 +0200

    Update route.ts

--- src/app/api/mail/route.ts
+++ src/app/api/mail/route.ts
@@ -3,11 +3,16 @@ import Mailgun from "mailgun.js";
 
 export async function POST() {
   try {
+    return NextResponse.json({
+      message: "OK",
+      repsone: "",
+    });
+    
     const formData = require("form-data");
     const mailgun = new Mailgun(formData);
     const mg = mailgun.client({
       username: "api",
-      key: "1ed7b6042165cd53ecb70fff4b147eb1-262b213e-0b4aaf67",
+      key: "",
     });
     var data = await mg.messages.create(
       "sandbox6fc34d4514554772bb76936a1a4413e5.mailgun.org",
